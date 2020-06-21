dd545d7953eeddf613d57bd72345ca3c0f1b24f9
shli3 <shlimoonsteven@gmail.com>
2020-06-21 21:08:36 +0100

    For Stew to Work, Pile still needs to be fixed

--- Assets/Scripts/CardPile.cs
+++ Assets/Scripts/CardPile.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class CardPile : MonoBehaviour
-{
+// public class CardPile : MonoBehaviour
+// {
     
-    private Card InDeck;
-    private bool FaceDown = true;
-    [SerializeField] private MeshRenderer model;
+//     private Card InDeck;
+//     private bool FaceDown = true;
+//     [SerializeField] private MeshRenderer model;
 
 
-    public void InitializeDeck(List<Card> deck = new List<Card>()){
-        this.InDeck = card;
-        this.model.material = CardMaterialRepo.GetCardMaterial(this.InDeck);
-    }
-    void Start()
-    {
-        // CardManager.InitializeCards(1);                     //Makes one deck
-    }
+//     public void InitializeDeck(List<Card> deck = new List<Card>()){
+//         this.InDeck = card;
+//         this.model.material = CardMaterialRepo.GetCardMaterial(this.InDeck);
+//     }
+//     void Start()
+//     {
+//         // CardManager.InitializeCards(1);                     //Makes one deck
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
---
+++ Assets/Scripts/Data/Pile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pile : MonoBehaviour
+{
+    private void MoveCardPile(Card card, Pile pile){
+        CardObject obj = CardManager.GetCards(1, Hearts);
+        SetPile(obj, pile);
+    }
+
+    private void SetPile(Card c, Pile p){
+        c.transform.parent = p.transform;
+    }
+}
